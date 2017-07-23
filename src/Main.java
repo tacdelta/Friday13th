89161a64a0908decc48f7997cb39277eef50fdad
tacdelta <tacdelta@mail.ru>
2017-07-23 14:23:44 +0300

    Cleaned Main, but now not operable in counting fridays

--- src/Main.java
+++ src/Main.java
@@ -8,41 +8,34 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println ( "Please input the start date.(mm/dd/yyyy)" );
+        UserDate startYear = new UserDate ();
+        startYear.getDate ();
 
-            System.out.print ( "Enter the year: " );
-            Scanner sc = new Scanner ( System.in );
-            String s = sc.next ();
-            do {
-                if (!isInteger ( s )) {
-                    System.out.println ("Please enter a number.");
-                    s = sc.next ();
+        System.out.println ( "Please input the end date.(mm/dd/yyyy)" );
+        UserDate endYear = new UserDate ();
+        endYear.getDate ();
+
+//        Calendar cal = new GregorianCalendar ( endYear.getDate(), 0, 13 );
+  //      Calendar cal = new GregorianCalendar ( year, 0, 13 );
+
+        //занесение в ДАТЕ
+
+
+    /*
+
+            Calendar cal = new GregorianCalendar ( year, 0, 13 );
+            for (int i = 0; i < 12; i++) {
+                int check = cal.get ( Calendar.DAY_OF_WEEK );
+                if (check == 6) {
+                    int tem = cal.get ( Calendar.MONTH );
+                    System.out.print ( formatMonth ( tem, Locale.ENGLISH ) + "\t" );
+                    System.out.println ( cal.get ( Calendar.DAY_OF_MONTH ) );
                 }
-            } while ((!isInteger ( s )));
-
-        int year = Integer.parseInt (s);
-        Calendar cal = new GregorianCalendar ( year, 0, 13 );
-        for (int i = 0; i < 12; i++) {
-            int check = cal.get ( Calendar.DAY_OF_WEEK );
-            if (check == 6) {
-                int tem = cal.get ( Calendar.MONTH );
-                System.out.print ( formatMonth ( tem, Locale.ENGLISH ) + "\t" );
-                System.out.println ( cal.get ( Calendar.DAY_OF_MONTH ) );
+                cal.add ( Calendar.MONTH, 1 );
             }
-            cal.add ( Calendar.MONTH, 1 );
         }
-    }
 
-    public static String formatMonth(int month, Locale locale) {
-        DateFormatSymbols symbols = new DateFormatSymbols ( locale );
-        String[] monthNames = symbols.getMonths ();
-        return monthNames[month];
-    }
-    static boolean isInteger(String s) {
-        try {
-            Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
+                */
     }
 }
